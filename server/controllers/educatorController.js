be7ea85d18925d33448352d795fa3ef96a4dc435
amoskipchumba254 <amokipchu@gmail.com>
2025-02-26 16:59:49 +0300

    new files and changes

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,5 +1,6 @@
 import { clerkClient } from '@clerk/express'
-
+import Course from '../models/Course.js'
+import { v2 as cloudinary } from 'cloudinary'
 
 // update role to educator
 export const updateRoleToEducator = async (req, res)=>{
@@ -16,5 +17,30 @@ export const updateRoleToEducator = async (req, res)=>{
 
     }catch (error){
         res.json({success: false, message: error.message})
+    }
+}
+
+// Add New Course
+export const addNewCourse = async (req, res)=>{
+    try {
+        const {courseData} = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+
+        if(!imageFile){
+            return res.json({success: false, message: 'Thumbnail Not Attached'})
+        }
+
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+        const newCourse = await Course.create(parsedCourseData)
+        const imageUpload = await connectCloudinary.uploader.upload(imageFile.path)
+        newCourse.courseThumbnail = imageUpload.secure_url
+        await newCourse.save()
+
+        res.json({ success: true, message: 'Course Added' })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { updateRoleToEducator } from '../controllers/educatorController.js'
+import { updateRoleToEducator } from '../controllers/educatorController.js';
+import upload from '../configs/multer.js';
+import { ProtectEducator } from '../middlewares/authMiddleware.js';
 
 const educatorRouter = express.Router()
 
 // Add Educator Role
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'), ProtectEducator, addCourse)
 
 export default educatorRouter
